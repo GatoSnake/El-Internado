36f2205263822ca9a07fb5dcb6b15d29574cfe29
rzentenoz <rodrigo.zenteno@agile.cl>
2015-12-14 16:37:27 -0300

    bloginfo description

--- webdesign/index.php
+++ webdesign/index.php
@@ -9,12 +9,7 @@
 			</div>
 			<div class="row">
 				<div class="col-xs-12">
-					<p class="text-center  class="f-atw"">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<p class="text-center f-atw"><?php bloginfo('description'); ?></p>
 				</div>
 			</div>
 		</div>
